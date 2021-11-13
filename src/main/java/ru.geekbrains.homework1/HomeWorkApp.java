c19015d5777daaf571c1aad03135ca28522ab7cf
lenazhuk <vaitler87@icloud.com>
2021-11-13 17:14:19 +0300

    homework2

--- src/main/java/ru.geekbrains.homework1/HomeWorkApp.java
+++ src/main/java/ru.geekbrains.homework1/HomeWorkApp.java
@@ -2,6 +2,52 @@ package ru.geekbrains.homework1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
+        //1
+        if (sum(3, 7)) {
+            System.out.println("sum >= 10 && sum <= 20");
+        } else {
+            System.out.println("!sum >= 10 && sum <= 20");
+        }
 
+        //2
+        number(7);
+
+        //3
+        if (comparison(-3)) {
+            System.out.println("отрицательное");
+        } else {
+            System.out.println("положительное");
+        }
+
+        //4
+        line("понял", 3);
+    }
+
+    //1
+    public static boolean sum(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+
+    //2
+    public static void number(int a) {
+        if (a % 2 == 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    //3
+    public static boolean comparison(int a) {
+        return a < 0;
+    }
+
+    //4
+    public static void line(String a, int b) {
+        for (int i = 0; i < b; i++) {
+            System.out.println(a);
+        }
     }
 }
+
