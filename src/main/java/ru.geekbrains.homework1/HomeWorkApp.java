28f7a22fccf69caa7babb6679ab8d16917687f96
lenazhuk <vaitler87@icloud.com>
2021-12-02 18:40:35 +0300

    homework7

--- src/main/java/ru.geekbrains.homework1/HomeWorkApp.java
+++ src/main/java/ru.geekbrains.homework1/HomeWorkApp.java
@@ -2,6 +2,18 @@ package ru.geekbrains.homework1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-
+        Cat[] cats = {
+                new Cat("pussy", 8),
+                new Cat("kisa", 2),
+                new Cat("borik", 5)
+        };
+        Plate plate = new Plate(10);
+        for (Cat cat : cats) {
+            cat.eat(plate);
+            System.out.println(cat);
+        }
+        plate.addFood(20);
+        cats[2].eat(plate);
+        System.out.println(cats[2]);
     }
 }
